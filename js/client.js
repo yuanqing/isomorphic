6c696e13f1b0389c1221d7e2938e759ff92bb8cd
yuanqing <desk@yuanqing.sg>
2015-10-02 20:54:55 +0800

    Remove `module.exports`

--- js/client.js
+++ js/client.js
@@ -5,7 +5,7 @@ var firebase = require('./firebase');
 var MainComponent = require('./components/main-component');
 var StoreActionCreator = require('./action-creators/store-action-creator');
 
-var mainComponent = React.render(<MainComponent state={window.__STATE__} />,
+React.render(<MainComponent state={window.__STATE__} />,
   document.querySelector('.__APP__'));
 delete window.__STATE__;
 var scriptElem = document.querySelector('.__STATE__');
@@ -15,5 +15,3 @@ firebase.listen(function(data) {
   console.log('Synced from Firebase:', data);
   store.dispatch(StoreActionCreator.receiveProducts(data.products));
 });
-
-module.exports = mainComponent;
