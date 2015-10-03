a1d6f6c80991beec9456f3d3a559f5f6803ed92b
yuanqing <desk@yuanqing.sg>
2015-10-03 17:03:31 +0800

    Edit config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
+// `envify` and `uglify` will cause the unused `if` branch to be removed.
+// Only the appropriate configuration file will be bundled and served.
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./production');
 } else {
-  module.exports = require('./development');
+  module.exports = require('./develop');
 }
