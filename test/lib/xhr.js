41226ef73fbb6f7694dbdc8632708c661115cd70
Yuan Qing <yuanqing.lim@honestbee.com>
2015-10-05 09:07:27 +0800

    Remove `nock`

--- test/lib/xhr.js
+++ test/lib/xhr.js
@@ -1,5 +1,4 @@
 var test = require('tape');
-var nock = require('nock');
 
 var xhr = require('../../lib/xhr');
 
@@ -7,29 +6,5 @@ test('has all the RESTful methods', function(t) {
   t.plan(6);
   ['get', 'post', 'put', 'head', 'patch', 'delete'].forEach(function(method) {
     t.true(typeof xhr[method] === 'function');
-  });
-});
-
-test('get', function(t) {
-  t.plan(1);
-  nock('https://foo.com')
-    .get('/')
-    .reply(200, { bar: 'baz' });
-  xhr.get('https://foo.com').then(function(result) {
-    t.looseEqual(result, { bar: 'baz' });
-  }, function() {
-    t.fail();
-  });
-});
-
-test('get, with `options.raw` set to `true`', function(t) {
-  t.plan(1);
-  nock('https://foo.com')
-    .get('/')
-    .reply(200, { bar: 'baz' });
-  xhr.get('https://foo.com', { raw: true }).then(function(result) {
-    t.looseEqual(result, '{"bar":"baz"}');
-  }, function() {
-    t.fail();
   });
 });
