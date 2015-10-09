ca3c8cfdc03cfbdacf58943a3a9c01c7de08ba85
Yuan Qing <yuanqing.lim@honestbee.com>
2015-10-09 14:38:35 +0800

    Extract link interception logic into `lib/link-interceptor`

--- js/client.js
+++ js/client.js
@@ -1,13 +1,19 @@
 var React = require('react');
 
+var linkInterceptor = require('../lib/link-interceptor');
+
 var store = require('./store');
 var firebase = require('./firebase');
-var Controller = require('./Controller');
-
+var Controller = require('./controller');
+var routeActionCreator = require('./action-creators/route-action-creator');
 var storeActionCreator = require('./action-creators/store-action-creator');
 
 React.render(<Controller state={window.__STATE__} />,
   document.querySelector('.app'));
+
+linkInterceptor(function(url, opts) {
+  store.dispatch(routeActionCreator.route(url, opts));
+});
 
 // Connect to Firebase and listen to changes.
 firebase.listen(function(data) {
