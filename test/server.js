d62aec54b883c45ae74c4f57c441f9e9634ea244
yuanqing <desk@yuanqing.sg>
2015-10-13 01:01:38 +0800

    Allow running the test server via `node test/server`

--- test/server.js
+++ test/server.js
@@ -1,6 +1,8 @@
 var cors = require('cors');
 var express = require('express');
 
+var config = require('../config');
+
 var app = express();
 
 app.get('/', cors(), function(req, res) {
@@ -11,5 +13,9 @@ app.all('*', cors(), function(req, res) {
   res.status(404);
   res.json({ foo: 'fail' });
 });
+
+if (require.main === module) {
+  app.listen(config.testPort);
+}
 
 module.exports = app;
